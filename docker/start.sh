aa9d6f7f69459415c25c7175fd2b1e9dac5b35a2
Fractal-Tess <vgfractal@gmail.com>
2025-08-04 11:55:09 +0300

    Update Docker configuration: modify dev Dockerfile and compose file, remove start.sh

--- docker/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Start lldb-server for remote debugging in the background
-echo "Starting lldb-server for remote debugging..."
-lldb-server platform --server --listen 0.0.0.0:12345 
-LLDB_PID=$!
-echo "LLDB server started with PID: $LLDB_PID"
