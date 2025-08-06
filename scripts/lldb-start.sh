deb5386d2dec3e9885fc5bd4b1647dea7fef3af3
Fractal-Tess <vgfractal@gmail.com>
2025-08-06 04:13:54 +0300

    chore: remove lldb-start script for remote debugging as it is no longer needed

--- scripts/lldb-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-# Start lldb-server for remote debugging in the background
-echo "Starting lldb-server for remote debugging..."
-lldb-server platform --server --listen *:12345 &
-LLDB_PID=$!
-
-# Wait for lldb-server to exit
-wait $LLDB_PID
