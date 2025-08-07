4c8e61076f785bc4eebb12350da44a265932b56d
Fractal-Tess <vgfractal@gmail.com>
2025-08-08 01:42:50 +0300

    refactor: remove old container and executor modules in favor of sandbox

--- src/container/mod.rs
+++
@@ -1,7 +0,0 @@
-mod container;
-mod filesystem;
-mod namespaces;
-
-pub use container::Container;
-pub use filesystem::ContainerFilesystem;
-pub use namespaces::ContainerNamespaces;
--- src/executor/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod pool;
-pub mod task;
-pub mod worker;
-
-pub use pool::ExecutorPool;
-pub use task::{Task, TaskResult};
-pub use worker::Worker;
