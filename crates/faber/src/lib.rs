43ac836555536571fda206ebd454247cc4c3a274
Fractal-Tess <vgfractal@gmail.com>
2025-08-09 14:38:11 +0300

    faber: declare runtime_builder mod and re-export RuntimeBuilder

--- crates/faber/src/lib.rs
+++ crates/faber/src/lib.rs
@@ -1,7 +1,9 @@
 mod error;
 mod prelude;
 mod runtime;
+mod runtime_builder;
 mod types;
 
 pub use runtime::Runtime;
-pub use types::{Mount, Task, TaskResult};
+pub use runtime_builder::RuntimeBuilder;
+pub use types::{CgroupConfig, Mount, Task, TaskResult};
