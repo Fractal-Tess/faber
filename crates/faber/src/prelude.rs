ee7187061224df970518ed08ed47b5088b168e83
Fractal-Tess <vgfractal@gmail.com>
2025-08-20 23:26:14 +0300

    feat: implement Closable trait for PipeReader and PipeWriter
    - Add Closable trait with a close method for PipeReader and PipeWriter
    - Implement error handling for closing file descriptors using nix::unistd::close
    - Enhance resource management by ensuring proper closure of pipe resources

--- crates/faber/src/prelude.rs
+++ crates/faber/src/prelude.rs
@@ -1,3 +1,34 @@
+use std::io::{PipeReader, PipeWriter};
+use std::os::fd::IntoRawFd;
+
 pub use crate::error::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+pub trait Closable {
+    fn close(self) -> Result<()>;
+}
+
+impl Closable for PipeReader {
+    fn close(self) -> Result<()> {
+        let fd = self.into_raw_fd();
+        nix::unistd::close(fd).map_err(|e| Error::Pipe {
+            details: format!("Failed to close fd of pipe reader: {fd}: {e}"),
+            error: e,
+        })?;
+
+        Ok(())
+    }
+}
+
+impl Closable for PipeWriter {
+    fn close(self) -> Result<()> {
+        let fd = self.into_raw_fd();
+        nix::unistd::close(fd).map_err(|e| Error::Pipe {
+            details: format!("Failed to close fd of pipe writer: {fd}: {e}"),
+            error: e,
+        })?;
+
+        Ok(())
+    }
+}
